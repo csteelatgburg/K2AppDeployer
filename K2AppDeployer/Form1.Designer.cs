9f60cb919d71e8cb4bb7fdba57f5adf021f4eab4
Chuck Steel <chuck@steelfarms.org>
2016-06-07 15:47:23 -0400

    Should be feature ready.

--- K2AppDeployer/Form1.Designer.cs
+++ K2AppDeployer/Form1.Designer.cs
@@ -28,11 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.K2HostName = new System.Windows.Forms.TextBox();
             this.K2SAMBAPass = new System.Windows.Forms.TextBox();
             this.mapKDrive = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.RichTextBox();
+            this.tasksList = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tasksListLabel = new System.Windows.Forms.Label();
+            this.POTasksLabel = new System.Windows.Forms.Label();
+            this.POTasks = new System.Windows.Forms.ListView();
+            this.TaskID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TaskName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.WorkingDirectory = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CommandLine = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Parameters = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PostTaskAction = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.KACETaskType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FileType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Type = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Guid = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.generateTasksXML = new System.Windows.Forms.Button();
+            this.generateSettingsXML = new System.Windows.Forms.Button();
+            this.launchKE = new System.Windows.Forms.Button();
+            this.config = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -47,7 +68,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 89);
+            this.label2.Location = new System.Drawing.Point(24, 88);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(186, 25);
             this.label2.TabIndex = 1;
@@ -57,32 +78,193 @@
             // 
             this.K2HostName.Location = new System.Drawing.Point(268, 32);
             this.K2HostName.Name = "K2HostName";
-            this.K2HostName.Size = new System.Drawing.Size(407, 31);
+            this.K2HostName.Size = new System.Drawing.Size(279, 31);
             this.K2HostName.TabIndex = 2;
             // 
             // K2SAMBAPass
             // 
-            this.K2SAMBAPass.Location = new System.Drawing.Point(269, 88);
+            this.K2SAMBAPass.Location = new System.Drawing.Point(268, 82);
             this.K2SAMBAPass.Name = "K2SAMBAPass";
             this.K2SAMBAPass.PasswordChar = '*';
-            this.K2SAMBAPass.Size = new System.Drawing.Size(405, 31);
+            this.K2SAMBAPass.Size = new System.Drawing.Size(279, 31);
             this.K2SAMBAPass.TabIndex = 3;
             // 
             // mapKDrive
             // 
-            this.mapKDrive.Location = new System.Drawing.Point(37, 140);
+            this.mapKDrive.Location = new System.Drawing.Point(29, 135);
             this.mapKDrive.Name = "mapKDrive";
-            this.mapKDrive.Size = new System.Drawing.Size(232, 69);
+            this.mapKDrive.Size = new System.Drawing.Size(232, 62);
             this.mapKDrive.TabIndex = 4;
             this.mapKDrive.Text = "Map K: to K2000";
             this.mapKDrive.UseVisualStyleBackColor = true;
             this.mapKDrive.Click += new System.EventHandler(this.mapKDrive_Click);
             // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(12, 667);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(650, 319);
+            this.outputBox.TabIndex = 6;
+            this.outputBox.Text = "";
+            // 
+            // tasksList
+            // 
+            this.tasksList.FormattingEnabled = true;
+            this.tasksList.ItemHeight = 25;
+            this.tasksList.Location = new System.Drawing.Point(12, 420);
+            this.tasksList.Name = "tasksList";
+            this.tasksList.Size = new System.Drawing.Size(638, 204);
+            this.tasksList.TabIndex = 7;
+            this.tasksList.Visible = false;
+            this.tasksList.SelectedIndexChanged += new System.EventHandler(this.tasksList_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 639);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Output Log";
+            // 
+            // tasksListLabel
+            // 
+            this.tasksListLabel.AutoSize = true;
+            this.tasksListLabel.Location = new System.Drawing.Point(12, 377);
+            this.tasksListLabel.Name = "tasksListLabel";
+            this.tasksListLabel.Size = new System.Drawing.Size(290, 25);
+            this.tasksListLabel.TabIndex = 9;
+            this.tasksListLabel.Text = "Select a tasks list from below";
+            this.tasksListLabel.Visible = false;
+            // 
+            // POTasksLabel
+            // 
+            this.POTasksLabel.AutoSize = true;
+            this.POTasksLabel.Location = new System.Drawing.Point(776, 21);
+            this.POTasksLabel.Name = "POTasksLabel";
+            this.POTasksLabel.Size = new System.Drawing.Size(182, 25);
+            this.POTasksLabel.TabIndex = 11;
+            this.POTasksLabel.Text = "Post-Install Tasks";
+            // 
+            // POTasks
+            // 
+            this.POTasks.CheckBoxes = true;
+            this.POTasks.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.TaskID,
+            this.TaskName,
+            this.WorkingDirectory,
+            this.CommandLine,
+            this.Parameters,
+            this.PostTaskAction,
+            this.KACETaskType,
+            this.FileType,
+            this.Type,
+            this.Guid});
+            this.POTasks.Location = new System.Drawing.Point(768, 65);
+            this.POTasks.Name = "POTasks";
+            this.POTasks.Size = new System.Drawing.Size(674, 806);
+            this.POTasks.TabIndex = 12;
+            this.POTasks.UseCompatibleStateImageBehavior = false;
+            this.POTasks.View = System.Windows.Forms.View.Details;
+            // 
+            // TaskID
+            // 
+            this.TaskID.Text = "ID";
+            this.TaskID.Width = 40;
+            // 
+            // TaskName
+            // 
+            this.TaskName.Text = "Task Name";
+            this.TaskName.Width = 250;
+            // 
+            // WorkingDirectory
+            // 
+            this.WorkingDirectory.Text = "WorkingDirectory";
+            this.WorkingDirectory.Width = 10;
+            // 
+            // CommandLine
+            // 
+            this.CommandLine.Text = "CommandLine";
+            // 
+            // Parameters
+            // 
+            this.Parameters.Text = "Parameters";
+            // 
+            // PostTaskAction
+            // 
+            this.PostTaskAction.Text = "PostTaskAction";
+            // 
+            // KACETaskType
+            // 
+            this.KACETaskType.Text = "KACETaskType";
+            // 
+            // FileType
+            // 
+            this.FileType.Text = "FileType";
+            // 
+            // Type
+            // 
+            this.Type.Text = "Type";
+            // 
+            // Guid
+            // 
+            this.Guid.Text = "Guid";
+            // 
+            // generateTasksXML
+            // 
+            this.generateTasksXML.Location = new System.Drawing.Point(29, 203);
+            this.generateTasksXML.Name = "generateTasksXML";
+            this.generateTasksXML.Size = new System.Drawing.Size(232, 62);
+            this.generateTasksXML.TabIndex = 13;
+            this.generateTasksXML.Text = "Generate tasks.xml";
+            this.generateTasksXML.UseVisualStyleBackColor = true;
+            this.generateTasksXML.Visible = false;
+            this.generateTasksXML.Click += new System.EventHandler(this.generateTasksXML_Click);
+            // 
+            // generateSettingsXML
+            // 
+            this.generateSettingsXML.Location = new System.Drawing.Point(553, 32);
+            this.generateSettingsXML.Name = "generateSettingsXML";
+            this.generateSettingsXML.Size = new System.Drawing.Size(203, 61);
+            this.generateSettingsXML.TabIndex = 14;
+            this.generateSettingsXML.Text = "Save Settings";
+            this.generateSettingsXML.UseVisualStyleBackColor = true;
+            this.generateSettingsXML.Click += new System.EventHandler(this.generateSettingsXML_Click);
+            // 
+            // launchKE
+            // 
+            this.launchKE.Location = new System.Drawing.Point(32, 271);
+            this.launchKE.Name = "launchKE";
+            this.launchKE.Size = new System.Drawing.Size(229, 69);
+            this.launchKE.TabIndex = 15;
+            this.launchKE.Text = "Launch KACE Engine";
+            this.launchKE.UseVisualStyleBackColor = true;
+            this.launchKE.Click += new System.EventHandler(this.launchKE_Click);
+            // 
+            // config
+            // 
+            this.config.Location = new System.Drawing.Point(768, 877);
+            this.config.Name = "config";
+            this.config.Size = new System.Drawing.Size(674, 273);
+            this.config.TabIndex = 16;
+            this.config.Text = resources.GetString("config.Text");
+            this.config.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1456, 624);
+            this.ClientSize = new System.Drawing.Size(1456, 998);
+            this.Controls.Add(this.config);
+            this.Controls.Add(this.launchKE);
+            this.Controls.Add(this.generateSettingsXML);
+            this.Controls.Add(this.generateTasksXML);
+            this.Controls.Add(this.POTasks);
+            this.Controls.Add(this.POTasksLabel);
+            this.Controls.Add(this.tasksListLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tasksList);
+            this.Controls.Add(this.outputBox);
             this.Controls.Add(this.mapKDrive);
             this.Controls.Add(this.K2SAMBAPass);
             this.Controls.Add(this.K2HostName);
@@ -103,6 +285,26 @@
         private System.Windows.Forms.TextBox K2HostName;
         private System.Windows.Forms.TextBox K2SAMBAPass;
         private System.Windows.Forms.Button mapKDrive;
+        private System.Windows.Forms.RichTextBox outputBox;
+        private System.Windows.Forms.ListBox tasksList;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label tasksListLabel;
+        private System.Windows.Forms.Label POTasksLabel;
+        private System.Windows.Forms.ListView POTasks;
+        private System.Windows.Forms.ColumnHeader TaskID;
+        private System.Windows.Forms.ColumnHeader TaskName;
+        private System.Windows.Forms.ColumnHeader WorkingDirectory;
+        private System.Windows.Forms.Button generateTasksXML;
+        private System.Windows.Forms.ColumnHeader CommandLine;
+        private System.Windows.Forms.ColumnHeader Parameters;
+        private System.Windows.Forms.ColumnHeader PostTaskAction;
+        private System.Windows.Forms.ColumnHeader KACETaskType;
+        private System.Windows.Forms.ColumnHeader FileType;
+        private System.Windows.Forms.ColumnHeader Type;
+        private System.Windows.Forms.ColumnHeader Guid;
+        private System.Windows.Forms.Button generateSettingsXML;
+        private System.Windows.Forms.Button launchKE;
+        private System.Windows.Forms.RichTextBox config;
     }
 }
 
--- K2AppDeployer/Form1.cs
+++ K2AppDeployer/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Xml;
 
 
 
@@ -24,27 +25,243 @@ namespace K2AppDeployer
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            if(Directory.Exists(@"K:\scripts"))
+            {
+                log("K: drive appears to be mapped to the K2000 already.");
+                getTaskFiles();
+            }
+            if (File.Exists(@"settings.xml"))
+            {
+                log("Loading settings from settings.xml");
+                string XMLFile = "settings.xml";
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(XMLFile);
+                XmlNode K2HostNameNode = xmlDoc.SelectSingleNode("//Settings/K2HostName");
+                K2HostName.Text = K2HostNameNode.InnerText;
+
+            }
+            if(File.Exists(@"tasks.xml"))
+            {
+                Directory.CreateDirectory(@"c:\KACE\engine");
+                File.Copy(@"tasks.xml", @"c:\KACE\engine\tasks.xml");
+            }
+            String[] arguments = Environment.GetCommandLineArgs();
+            if(arguments.Length > 1)
+            {
+                K2SAMBAPass.Text = arguments[1];
+                if(K2HostName.Text != "") {
+                    mapDrive();
+                }
+            }
 
         }
 
-        private void mapKDrive_Click(object sender, EventArgs e)
+        public void log(string message)
         {
+            outputBox.Text = outputBox.Text + message + "\r\n";
+        }
+
+        public void mapDrive()
+        {
+            log("Mapping k drive to K2 peinst share");
             Cursor.Current = Cursors.WaitCursor;
             string userName = "admin";
             string K2path = "\\\\" + K2HostName.Text + "\\peinst";
             if (DriveSettings.MapNetworkDrive("K", K2path, userName, K2SAMBAPass.Text))
             {
-                System.Diagnostics.Process.Start("explorer.exe", @"K:\");
+
+                log("Mapped K: to " + K2path);
+                //System.Diagnostics.Process.Start("explorer.exe", @"K:\");
+                getTaskFiles();
 
             }
             else
             {
                 //there was a problem, I guess.
                 MessageBox.Show("Error mapping K: to " + K2path);
-                
+
             }
             Cursor.Current = Cursors.Default;
 
+        }
+
+        private void mapKDrive_Click(object sender, EventArgs e)
+        {
+            mapDrive();
+        }
+
+        public void getTaskFiles()
+        {
+            tasksList.Visible = true;
+            tasksListLabel.Visible = true;
+            log("Getting list of task files");
+            var taskFiles = Directory.GetFiles(@"K:\scripts", "*Tasks*.xml").OrderBy(f => f);
+            foreach (string file in taskFiles)
+                {
+                    tasksList.Items.Add(file);
+                }
+
+}
+
+        private void tasksList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            POTasks.Items.Clear();
+            var unCheckedTasks = new List<string>();
+            unCheckedTasks.Add("Set KACE Path");
+            unCheckedTasks.Add("Fix BCD");
+            unCheckedTasks.Add("Copy Drivers");
+            unCheckedTasks.Add("Copy Post Install Files");
+            unCheckedTasks.Add("Setup Post Install Tasks");
+            unCheckedTasks.Add("Update Configuration For OS Tasks");
+            unCheckedTasks.Add("Capture and Disable UAC");
+            unCheckedTasks.Add("Run DPInst");
+            unCheckedTasks.Add("Restore UAC");
+            
+            log("Selected " + tasksList.SelectedItem);
+            string XMLFile = tasksList.SelectedItem.ToString();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(XMLFile);
+            XmlNodeList itemNodes = xmlDoc.SelectNodes("//Tasks/Task");
+            foreach (XmlNode itemNode in itemNodes)
+            {
+                string id = itemNode.Attributes["ID"].Value;
+                XmlNode name = itemNode.SelectSingleNode("Name");
+                XmlNode WorkingDirectory = itemNode.SelectSingleNode("WorkingDirectory");
+                XmlNode CommandLine = itemNode.SelectSingleNode("CommandLine");
+                XmlNode Parameters = itemNode.SelectSingleNode("Parameters");
+                XmlNode PostTaskAction = itemNode.SelectSingleNode("PostTaskAction");
+                XmlNode KACETaskType = itemNode.SelectSingleNode("KACETaskType");
+                XmlNode FileType = itemNode.SelectSingleNode("FileType");
+                XmlNode Type = itemNode.SelectSingleNode("Type");
+                XmlNode Guid = itemNode.SelectSingleNode("Guid");
+
+                if (Type.InnerText == "PO")
+                {
+                    ListViewItem itm;
+                    string[] task = new string[10];
+                    task[0] = id;
+                    task[1] = name.InnerText;
+                    task[2] = WorkingDirectory.InnerText;
+                    task[3] = CommandLine.InnerXml;
+                    task[4] = Parameters.InnerText;
+                    task[5] = PostTaskAction.InnerText;
+                    task[6] = KACETaskType.InnerText;
+                    task[7] = FileType.InnerText;
+                    task[8] = Type.InnerText;
+                    task[9] = Guid.InnerText;
+                    itm = new ListViewItem(task);
+                    if (unCheckedTasks.Contains(name.InnerText))
+                    {
+                        POTasks.Items.Add(itm);
+                    } else
+                    {
+                        itm.Checked = true;
+                        POTasks.Items.Add(itm);
+                    }
+                    
+                }
+            }
+            generateTasksXML.Visible = true;
+
+        }
+
+        private void generateTasksXML_Click(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(@"c:\KACE\engine");
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlNode rootNode = xmlDoc.CreateElement("Tasks");
+            xmlDoc.AppendChild(rootNode);
+            foreach (ListViewItem task in POTasks.Items)
+            {
+                if(task.Checked)
+                {
+                    XmlNode taskNode = xmlDoc.CreateElement("Task");
+                    XmlAttribute attribute = xmlDoc.CreateAttribute("ID");
+                    attribute.Value = task.SubItems[0].Text;
+                    XmlNode name = xmlDoc.CreateElement("Name");
+                    name.InnerText = task.SubItems[1].Text;
+                    taskNode.AppendChild(name);
+                    XmlNode WorkingDirectory = xmlDoc.CreateElement("WorkingDirectory");
+                    if(Directory.Exists("K:\\applications\\" + task.SubItems[0].Text + "\\contents"))
+                    {
+                        WorkingDirectory.InnerText = task.SubItems[2].Text + "\\contents";
+                    } else
+                    {
+                        WorkingDirectory.InnerText = task.SubItems[2].Text;
+                    }
+                    taskNode.AppendChild(WorkingDirectory);
+                    XmlNode CommandLine = xmlDoc.CreateElement("CommandLine");
+                    CommandLine.InnerXml = task.SubItems[3].Text;
+                    taskNode.AppendChild(CommandLine);
+                    XmlNode Parameters = xmlDoc.CreateElement("Parameters");
+                    Parameters.InnerText = task.SubItems[4].Text;
+                    taskNode.AppendChild(Parameters);
+                    XmlNode PostTaskAction = xmlDoc.CreateElement("PostTaskAction");
+                    PostTaskAction.InnerText = task.SubItems[5].Text;
+                    taskNode.AppendChild(PostTaskAction);
+                    XmlNode KACETaskType = xmlDoc.CreateElement("KACETaskType");
+                    KACETaskType.InnerText = task.SubItems[6].Text;
+                    taskNode.AppendChild(KACETaskType);
+                    XmlNode FileType = xmlDoc.CreateElement("FileType");
+                    FileType.InnerText = task.SubItems[7].Text;
+                    taskNode.AppendChild(FileType);
+                    XmlNode Type = xmlDoc.CreateElement("Type");
+                    Type.InnerText = task.SubItems[8].Text;
+                    taskNode.AppendChild(Type);
+                    XmlNode Guid = xmlDoc.CreateElement("Guid");
+                    Guid.InnerText = task.SubItems[9].Text;
+                    taskNode.AppendChild(Guid);
+                    rootNode.AppendChild(taskNode);
+                }
+            }
+            xmlDoc.Save(@"c:\KACE\engine\tasks.xml");
+            log("Saved tasks.xml file");
+
+            //System.Diagnostics.Process.Start("notepad.exe", @"c:\temp\tasks.xml");
+        }
+
+        private void generateSettingsXML_Click(object sender, EventArgs e)
+        {
+            XmlDocument settings = new XmlDocument();
+            XmlNode rootNode = settings.CreateElement("Settings");
+            settings.AppendChild(rootNode);
+            XmlNode K2HostNameNode = settings.CreateElement("K2HostName");
+            K2HostNameNode.InnerText = K2HostName.Text;
+            rootNode.AppendChild(K2HostNameNode);
+            settings.Save("settings.xml");
+        }
+
+        private void launchKE_Click(object sender, EventArgs e)
+        {
+            bool is64 = Environment.Is64BitOperatingSystem;
+            log(@"Copying KACE Engine files to c:\KACE");
+            Directory.CreateDirectory(@"c:\KACE\engine");
+            if(!File.Exists(@"c:\KACE\engine\tasks.xml"))
+            {
+                MessageBox.Show("No tasks.xml file was found, please select a list of tasks and generate one.");
+                
+            }
+            if (!Directory.Exists(@"k:\scripts\"))
+            {
+                mapDrive();
+            }
+            string[] configXML = { config.Text.ToString() };
+            File.WriteAllLines(@"C:\KACE\engine\config.xml", configXML);
+            if(is64)
+            {
+                File.Copy(@"K:\hta\amd64\KACEEngine.exe", @"C:\KACE\engine\KACEEngine.exe", true);
+
+            } else
+            {
+                File.Copy(@"K:\hta\x86\KACEEngine.exe", @"C:\KACE\engine\KACEEngine.exe", true);
+            }
+            DirectoryCopyClass.DirectoryCopy(@"K:\hta\kaceengineui", @"c:\KACE\engine\kaceengineui", true);
+            System.Diagnostics.Process.Start(@"c:\KACE\engine\KACEEngine.exe");
+        }
+
+        private void config_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
@@ -169,5 +386,52 @@ public class DriveSettings
             }
         }
         return false;
+    }
+}
+
+public class DirectoryCopyClass
+{
+    static void DCMain()
+    {
+        // Copy from the current directory, include subdirectories.
+        DirectoryCopy(".", @".\temp", true);
+    }
+
+    public static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+    {
+        // Get the subdirectories for the specified directory.
+        DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+        if (!dir.Exists)
+        {
+            throw new DirectoryNotFoundException(
+                "Source directory does not exist or could not be found: "
+                + sourceDirName);
+        }
+
+        DirectoryInfo[] dirs = dir.GetDirectories();
+        // If the destination directory doesn't exist, create it.
+        if (!Directory.Exists(destDirName))
+        {
+            Directory.CreateDirectory(destDirName);
+        }
+
+        // Get the files in the directory and copy them to the new location.
+        FileInfo[] files = dir.GetFiles();
+        foreach (FileInfo file in files)
+        {
+            string temppath = Path.Combine(destDirName, file.Name);
+            file.CopyTo(temppath, false);
+        }
+
+        // If copying subdirectories, copy them and their contents to new location.
+        if (copySubDirs)
+        {
+            foreach (DirectoryInfo subdir in dirs)
+            {
+                string temppath = Path.Combine(destDirName, subdir.Name);
+                DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+            }
+        }
     }
 }
